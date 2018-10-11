ee2f59ec55ac105733feb09c108a7b5f94248eb5
Keith McSpurren <info@track2development.com>
2018-10-11 13:26:37 -0400

    progress.  getting display and labels correct.  math is very wrong.

--- mainfinal2rf.js
+++ mainfinal2rf.js
@@ -21,7 +21,7 @@ const fakedata = [[2, 3, 5], [4, 4, 7], [2, 9, 5], [2, 3, 5], [4, 4, 4], [2, 1, 
 let yDataSets = fakedata[0].length;
 
 // Object for chart options
-let optionsObj = {
+let threeStackedBar = {
   fakedata : [[2, 3, 5], [4, 4, 7], [2, 9, 5], [2, 3, 5], [4, 4, 4], [2, 1, 5], [2, 3, 5]],
   xaxis : ['Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
   arrayLength() {
@@ -42,13 +42,13 @@ let optionsObj = {
 
 //starter function starts all the functions working together
 function start() {
-  math();
-  yTickMaker();
+  m(threeStackedBar);
+  tickLabels(threeStackedBar);
   tableMaker();
-  createBars();
-  labelsAndTitles();
+  createBars(threeStackedBar);
+  labelsAndTitles(threeStackedBar);
   tablePadding();
-  yTicksAndTitle();
+  yTicksAndTitle(threeStackedBar);
 
 }
 // there are 6 main functions:  math, tableMaker, createBars...
@@ -63,14 +63,14 @@ function start() {
 // conservatism on space) and then provides an integer which is set as the px width of all the bars
 // the key variables are stored outside the math function as the varaible releasedData.
 // if tests to see if the data is single dimension array (simple bar chart) or 2 dimensional (stacked bar)
-function math () {
+let m = function (optionsObj) {
   let totals = [];
   let sum = 0;
 
-  if(fakedata[0].constructor === Array) {
-  for (i = 0; i < fakedata.length; i++) {
-    for (a = 0; a < yDataSets; a++) {
-      sum += fakedata[i][a];
+  if(optionsObj.fakedata[0].constructor === Array) {
+  for (i = 0; i < optionsObj.fakedata.length; i++) {
+    for (a = 0; a < optionsObj.arrayLength(); a++) {
+      sum += optionsObj.fakedata[i][a];
     } totals.push(sum);
       sum = 0;
   }
@@ -79,9 +79,9 @@ function math () {
   let percentSize = [];
   let fakeDataSize = [];
   let widthOfBar = (optionsObj.chartWidth * 0.9) / totals.length;
-  for (i = 0; i < fakedata.length; i++) {
-    for (a = 0; a < yDataSets; a++) {
-        percentSize.push(fakedata[i][a] / biggestNum);
+  for (i = 0; i < optionsObj.fakedata.length; i++) {
+    for (a = 0; a < optionsObj.arrayLength(); a++) {
+        percentSize.push(optionsObj.fakedata[i][a] / biggestNum);
         }
         fakeDataSize.push(percentSize);
         percentSize = [];
@@ -92,27 +92,25 @@ function math () {
 
   else {
 
-  let biggestNum = Math.max(...fakedata);
+  let biggestNum = Math.max(...optionsObj.fakedata);
   let percentSize = [];
   let fakeDataSize = [];
-  let widthOfBar = (optionsObj.chartWidth * 0.9) / fakedata.length;
-  for (i = 0; i < fakedata.length; i++) {
-        fakeDataSize.push(fakedata[i] / biggestNum);
+  let widthOfBar = (optionsObj.chartWidth * 0.9) / optionsObj.fakedata.length;
+  for (i = 0; i < optionsObj.fakedata.length; i++) {
+        fakeDataSize.push(optionsObj.fakedata[i] / biggestNum);
     }
    const cleanData = [biggestNum, fakeDataSize, widthOfBar];
     return cleanData;
   }
 }
 
-let releasedData = math();
-
 // function: yTickMaker uses biggestNum and creates an array of tickmarks from 0 to biggestNumb
 // for use on the Y axis. new function yTicks added which creates a y axis data label system
 // starting at zero to biggestNum at 25% increments rounded to nearest INT.
-function yTickMaker () {
+let tickLabels = function (optionsObj) {
   let x = [];
     for (i = 0; i <= 4; i++) {
-      x.push(Math.round(releasedData[0] * i * 0.25));
+      x.push(Math.round(m(optionsObj)[0] * i * 0.25));
     }
       let a = x.reverse();
       let y = a.toString();
@@ -120,7 +118,7 @@ function yTickMaker () {
 
     return tickArray;
 }
-let tickLabels = yTickMaker();
+
 
 
 
@@ -142,21 +140,21 @@ function tableMaker() {
 // function createBars: creates divs of different colors and sizes based on data provided.  TDs are created and then
 // each TD is appended with a specifc div with a unique id.  color, height and width are set by math function.
 // data labels are appened to divs via creaateTextNode getting labels from data set.
-function createBars() {
+function createBars(optionsObj) {
 
-  if(fakedata[0].constructor === Array) {
+  if(optionsObj.fakedata[0].constructor === Array) {
 
-  for (x = 0; x < fakedata.length; x++) {
+  for (x = 0; x < optionsObj.fakedata.length; x++) {
     let a = document.createElement("TD");
     a.setAttribute("id", "bars");
     a.setAttribute("style", "font-size: " + optionsObj.xTicks[0] + "px; color: " + optionsObj.xTicks[1]);
-  for (i = 0; i < yDataSets; i++) {
+  for (i = 0; i < optionsObj.arrayLength(); i++) {
     let b = document.createElement("DIV");
-    let c = document.createTextNode(fakedata[x][i]);
+    let c = document.createTextNode(optionsObj.fakedata[x][i]);
     b.appendChild(c);
     b.setAttribute("class", "rectangle");
     b.setAttribute("id", "div" + x + i);
-    b.setAttribute("style", "width: " + releasedData[2] + "px; height: " + releasedData[1][x][i] * optionsObj.chartHeight + "px; " + "background-color:" + optionsObj.barColor[i]);
+    b.setAttribute("style", "width: " + m(optionsObj)[2] + "px; height: " + m(optionsObj)[1][x][i] * optionsObj.chartHeight + "px; " + "background-color:" + optionsObj.barColor[i]);
     a.appendChild(b);
     document.getElementById("myTr1").appendChild(a);
     }
@@ -172,7 +170,7 @@ function createBars() {
     b.appendChild(c);
     b.setAttribute("class", "rectangle");
     b.setAttribute("id", "div" + x);
-    b.setAttribute("style", "width: " + releasedData[2] + "px; height: " + releasedData[1][x] * optionsObj.chartHeight + "px; " + "background-color:" + optionsObj.barColor[0]);
+    b.setAttribute("style", "width: " + m(optionsObj)[2] + "px; height: " + m(optionsObj)[1][x] * optionsObj.chartHeight + "px; " + "background-color:" + optionsObj.barColor[0]);
     a.appendChild(b);
     document.getElementById("myTr1").appendChild(a);
     }
@@ -181,9 +179,9 @@ function createBars() {
 
 // function labelsAndTitles: creates TDs for corresponding table rows for titling of xaxis, main chart, xaxis
 //labels.  jquery prepend method is used to add TDs for the yaxis title and yticks.  css used to rotate text.
-function labelsAndTitles() {
+function labelsAndTitles(optionsObj) {
   //xaxis labels
-  for (x = 0; x < fakedata.length; x++) {
+  for (x = 0; x < optionsObj.fakedata.length; x++) {
     let b = document.createElement("TD");
     let c = document.createTextNode(xaxis[x]);
     b.setAttribute("style", "font-size: " + optionsObj.xLabelFormat[0] + "px; text-align: " + optionsObj.xLabelFormat[2] + "; color: " + optionsObj.xLabelFormat[1]);
@@ -194,7 +192,7 @@ function labelsAndTitles() {
   //main chart title
   let e = document.createElement("TD");
   e.setAttribute("id", "chartTitle");
-  e.setAttribute("colspan", fakedata.length);
+  e.setAttribute("colspan", optionsObj.fakedata.length);
   e.setAttribute("style", "font-size: " + optionsObj.chartTitle[1] + "px; color: " + optionsObj.chartTitle[2] + "; text-align: " + optionsObj.chartTitle[3]);
   let f = document.createTextNode(optionsObj.chartTitle[0]);
   e.appendChild(f);
@@ -203,18 +201,18 @@ function labelsAndTitles() {
   //xaxis title
   let a = document.createElement("TD");
   a.setAttribute("id", "xTitle");
-  a.setAttribute("colspan", fakedata.length);
+  a.setAttribute("colspan", optionsObj.fakedata.length);
   a.setAttribute("style", "font-size: " + optionsObj.xAxisTitle[1] + "px; color: " + optionsObj.xAxisTitle[2] + "; text-align: " + optionsObj.xAxisTitle[3]);
   let d = document.createTextNode(optionsObj.xAxisTitle[0]);
   a.appendChild(d);
   document.getElementById("myTr3").appendChild(a);
 }
 
-function yTicksAndTitle () {
+function yTicksAndTitle (optionsObj) {
 
   // y ticklabels
   $('#bigTable #myTr1').eq(0).prepend('<td id="ycontainer"><p id="yticks"></p></td>');
-  document.getElementById("yticks").innerHTML = tickLabels;
+  document.getElementById("yticks").innerHTML = tickLabels(optionsObj);
   $('#yticks').css({"color": optionsObj.yTicks[1], "font-size": optionsObj.yTicks[0]});
 
   //y axis title
